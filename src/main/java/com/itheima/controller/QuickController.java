5b6d401c805cc5482f6d789fabf993f8a17adb99
zhangsan <zhangsan>
2020-02-23 12:45:26 +0800

    aaa

---
+++ src/main/java/com/itheima/controller/QuickController.java
@@ -0,0 +1,36 @@
+package com.itheima.controller;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ConfigurationProperties(prefix = "person")
+public class QuickController {
+
+    private String name;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @RequestMapping("/quick")
+    @ResponseBody
+    public String quick(){
+        return "name:"+name+",age:"+age;
+    }
+}
